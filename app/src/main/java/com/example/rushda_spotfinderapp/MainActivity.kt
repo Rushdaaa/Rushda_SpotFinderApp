b17717016f51a5cf4e0b32241390251a330cf485
Rushdaaa <rushda.khan@ontariotechu.net>
2025-11-02 19:23:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rushda_spotfinderapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.rushda_spotfinderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Map Fragment
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Center on Toronto
+        val toronto = LatLng(43.6532, -79.3832)
+        map.addMarker(MarkerOptions().position(toronto).title("Toronto"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(toronto, 12f))
+    }
+}
